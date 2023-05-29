5702eb07c2d4669ce4658448712f7a42bc6df34d
Cristian Buitrago <cristian@Cristians-MacBook-Pro.local>
2023-05-28 22:25:13 -0500

    time to solve the challenge

--- page/js/productScript.js
+++ page/js/productScript.js
@@ -9,26 +9,15 @@ const init = async () => {
   // esto lo podemos realizar utilizando el `window.location.search` del cual podremos obtener el id
   // 1.2 llamar la api https://fakestoreapi.com y guardar los resultados en una constante llamada `result`.
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const id = urlParams.get("id");
+  const id = "";
 
-  const data = await fetch(`https://fakestoreapi.com/products/${id}`);
-  const { title, category, description, image, price } = await data.json();
+  const result = "...";
 
   // 2. con la data del producto inyecta o remplaza la información que se encuentra en el HTML por la información obtenida de la API
   // te recomiendo que revises el elemento <div class="small-container single-product"> en el encontraras todos los elementos que debes
   // modificar con Javascript y asi mostrar la información correcta
 
-  const titleElement = document.getElementById("title");
-  titleElement.innerHTML = title;
-  const categoryElement = document.getElementById("category");
-  categoryElement.innerHTML = category;
-  const descriptionElement = document.getElementById("description");
-  descriptionElement.innerHTML = description;
-  const imageElement = document.getElementById("image");
-  imageElement.src = image;
-  const priceElement = document.getElementById("price");
-  priceElement.innerHTML = `$${price}`;
+  const titleElement = "...";
 
   // 3. agrega evento al botón `add to card` de tal manera que cuando se presione se emita una window alarm con el mensaje
   // `se tiene agregados ${numero de elementos agregados} con valor total ${el precio del producto multiplicado por la cantidad}`
@@ -36,20 +25,8 @@ const init = async () => {
   // 3.2 obtén del documento el botón `add to card` y agrega el evento para emitir el window alarm.
   // 3.3 si el valor es negativo emitir en el window alarm el mensaje `valores negativos no permitidos`
 
-  const button = document.getElementById("btn");
-  const quantity = document.getElementById("quantity");
-
-  button.addEventListener("click", () => {
-    if (quantity.value > 0) {
-      alert(
-        `se tiene agregados ${quantity.value} con valor total ${
-          quantity.value * price
-        }`
-      );
-    } else {
-      alert("valores negativos no permitidos");
-    }
-  });
+  const button = "...";
+  const quantity = "...";
 };
 
 init();
--- page/js/script.js
+++ page/js/script.js
@@ -5,8 +5,7 @@ const init = async () => {
   // 1. obtener todos los productos a mostrar, para esto debes llamar la api https://fakestoreapi.com/products
   // y guardar los resultados en una constante llamada `result`
 
-  const data = await fetch("https://fakestoreapi.com/products");
-  const result = await data.json();
+  const result = "...";
 
   // 2. dada la estructura de las tarjetas de los productos en la función card, renderizar los productos obtenidos
   // del llamado API, para esto debes recordar que el resultado del llamado API es un array que se debe recorrer con
@@ -49,21 +48,14 @@ const init = async () => {
     </div>
   </div>`;
 
-  const arrayCards = result.map(
-    ({ id, title, category, description, image, price }) =>
-      card(id, title, category, description, image, price)
-  );
+  const arrayCards = "...";
 
   // 3. inyecta el array de elementos construidos dentro del contenedor que sera el <div class="best-seller">, para esto:
   // 3.1 recuerda obtener el elemento del document
   // 3.2 convierte el array de cards a un único string que agrupe todas las cards utilizando el método `reduce`
   // 3.3 inyecte el string obtenido en el contenedor utilizando el método `innerHTML`
-  const productsContainer = document.getElementsByClassName("best-seller")[0];
-  const stringArrayCards = arrayCards.reduce(
-    (acc, elemento) => acc + elemento,
-    ""
-  );
-  productsContainer.innerHTML = stringArrayCards;
+  const productsContainer = "...";
+  const stringArrayCards = "...";
 };
 
 // se inicializa el script
